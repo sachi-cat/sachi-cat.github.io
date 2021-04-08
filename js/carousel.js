db6cd5a18421586732ec1744e42c8a7bd6336230
sachi-cat <81925961+sachi-cat@users.noreply.github.com>
2021-04-08 12:48:10 +0900

    Add files via upload

---
+++ js/carousel.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.carousel').slick({
+        autoplay: true, // 自動再生
+        autoplaySpeed: 5000, // 自動再生の速さ、単位はミリ秒
+        // appendDots: ,
+        // centerMode: true, //コメントアウトする
+        dots: true,
+        // fade: teue,
+        // mobileFirst: ,
+        arrows: false, // 左右のナビゲーションを無効にする・追加
+    });
+});
